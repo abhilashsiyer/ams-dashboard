ba2b3b765283e3e5281e94bcacf9955787e8558a
AIyer <abhilashsiyer@gmail.com>
2023-03-23 10:36:23 +1100

    fixed link

--- src/components/TestResults/Result/index.js
+++ src/components/TestResults/Result/index.js
@@ -6,37 +6,38 @@ const Result = ({ testCaseName, deviceTestResults, matriceId, projectId }) => {
 
   return (
     <div className="result">
-      <div className="thumb">
-        <Link to={`/projects/${projectId}/matrices/${matriceId}/testCase/${testCaseName}`}>
+      <Link
+        to={`/projects/${projectId}/matrices/${matriceId}/testCase/${testCaseName}`}>
+        <div className="thumb">
           <h3 className="testNameTitle">{testCaseName}</h3>
-          {/* <img src={testGif} alt={testName} /> */}
-        </Link>
-      </div>
+        </div>
 
-      <div className="details">
-        <ul>
-          <li>
-            <span className="name">
-              {deviceTestResults.map((deviceResult) => {
-                const { deviceName, deviceInfo, result } = deviceResult[0];
-                return (
-                  <Link to={`/projects/${projectId}/matrices/${matriceId}/testCase/${testCaseName}`}>
-                      <h3 className= "testResultDescription" key = {deviceName}>{`${deviceInfo.manufacturer} ${deviceInfo.name}`} {" - "} {result}</h3>
-                  </Link>
-                )
-              })}
-              {/* <Link to={`/testResult/${testCaseName}`}>
+        <div className="details">
+          <ul>
+            <li>
+              <span className="name">
+                {deviceTestResults.map((deviceResult) => {
+                  const { deviceName, deviceInfo, result } = deviceResult[0];
+                  return (
+                    <h3 className="testResultDescription" key={deviceName}>
+                      {`${deviceInfo.manufacturer} ${deviceInfo.name}`} {" - "}{" "}
+                      {result}
+                    </h3>
+                  );
+                })}
+                {/* <Link to={`/testResult/${testCaseName}`}>
                 <Button>{testResult}</Button>
               </Link> */}
-            </span>
-          </li>
-          {/* <li key="{name}">
+              </span>
+            </li>
+            {/* <li key="{name}">
             <span className="price">
               {testResult}
             </span>
           </li> */}
-        </ul>
-      </div>
+          </ul>
+        </div>
+      </Link>
     </div>
   );
 };
